7334b75b3ae5a2409df0819876d5e6f399f733cd
phonetical <masturcheif117@gmail.com>
2026-08-02 15:20:05 -0400

    stuff

--- src/main.c
+++ src/main.c
@@ -1,8 +1,33 @@
+#include <stdio.h>
+#include <sys/socket.h>
 #include <unistd.h>
 #include "server.h"
+#include <netinet/in.h>
 
 int main(int argc, char* argv[]) {
-    int tcp_fd = create_tcp_socket(8080, "0.0.0.0");
+    struct sockaddr_in client_sockaddr_in;
+    const int port = 8080;
+    const char* bind_addr = "0.0.0.0";
+    socklen_t len = sizeof(client_sockaddr_in);
+
+    int tcp_fd = create_tcp_socket(port, bind_addr);
+
+    listen(tcp_fd, 5);
+
+    fprintf(stderr, "Listening on %s:%d\n", bind_addr, port);
+
+    while (1) {
+        printf("new\n");
+        int conn_fd = accept(tcp_fd, (struct sockaddr*)&client_sockaddr_in, &len);
+
+        char buffer[256] = {};
+        read(conn_fd, buffer, sizeof(buffer));
+
+        printf("%s", buffer);
+
+        char status = 0;
+        write(conn_fd, &status, 1);
+    }
 
     close(tcp_fd);
 
--- src/server.c
+++ src/server.c
@@ -9,31 +9,19 @@
 
 int create_tcp_socket(const int port, const char* bind_addr) {
     struct sockaddr_in server_sockaddr_in;
-    struct sockaddr_in client_sockaddr_in;
-    socklen_t len = sizeof(client_sockaddr_in);
     int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
 
     server_sockaddr_in.sin_family = AF_INET;
     server_sockaddr_in.sin_port = htons(port);
     inet_pton(AF_INET, bind_addr, &server_sockaddr_in.sin_addr.s_addr);
 
+    int on = 1;
+    setsockopt(socket_fd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
+
     if (bind(socket_fd, (struct sockaddr *)&server_sockaddr_in, sizeof(server_sockaddr_in)) == -1) {
-        printf("bind: %s\n", strerror(errno));
+        fprintf(stderr, "bind: %s\n", strerror(errno));
         return 1;
     }
-
-    listen(socket_fd, 5);
-
-    printf("Listening on %s:%d\n", bind_addr, port);
-
-    int conn_fd = accept(socket_fd, (struct sockaddr *)&client_sockaddr_in, &len);
-
-    char buffer[256] = {};
-    read(conn_fd, buffer, sizeof(buffer));
-    printf("%s", buffer);
-
-    char status = 0;
-    write(conn_fd, &status, 1);
 
     return socket_fd;
 }
